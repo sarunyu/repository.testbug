001810adbb4a405038a538e60734487449526d34
testbug <try2hack@msn.com>
2019-05-30 22:19:06 +0700

    edit repo

--- script.hello.spittze/addon.py
+++ script.hello.spittze/addon.py
@@ -11,7 +11,7 @@ mac = ':'.join('%02X' % ((uuid.getnode() >> 8*i) & 0xff) for i in reversed(xrang
 #hostname = socket.gethostname()    
 #ip = socket.gethostbyname(hostname)  
 #get data
-response = urllib.urlopen('https://m0z.xyz/login/getmx.php?mac='+mac)
+response = urllib.urlopen('https://hello.roundtable-solution.com/spittze/getmx.php?mac='+mac)
 data = json.load(response)
 #varirable from database
 room = data['room']
@@ -19,12 +19,12 @@ name = data['name']
 user = data['user']
 pwd  = data['pwd']
 stat = data['stats']
-#text to display
+#text to display1
 title = "Welcome "+name+" to Spittze Hotel"
 line1 = "The hotel has free wifi"
 line2 = "Username : "+user+" and Password : "+pwd
 line3 = "Have a good stay. Thank you."
-
+#text to display2
 dev1 = "Room is check-out"
 dev2 = "Room Number "+room
 dev3 = "MAC : "+mac
